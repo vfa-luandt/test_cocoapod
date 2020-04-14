4bf0465d3e2cff22231e9b67167fd5409294bf9c
vfa.luandt <vfa.luandt@gmail.com>
2020-04-14 17:37:14 +0700

    itnit

---
+++ test/test.h
@@ -0,0 +1,19 @@
+//
+//  test.h
+//  test
+//
+//  Created by Dang Thanh Luan on 4/14/20.
+//  Copyright © 2020 Dang Thanh Luan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for test.
+FOUNDATION_EXPORT double testVersionNumber;
+
+//! Project version string for test.
+FOUNDATION_EXPORT const unsigned char testVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <test/PublicHeader.h>
+
+
